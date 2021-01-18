d16451f6ea5fc51e5f1974d0436bbb5ac5901597
Alan <you@example.comalanlichen1@gmail.com>
2021-01-17 21:48:21 -0800

    Add package.json file and start script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Todo: add readme
+# Roblox Game Statistics
+
+This is a react app that is used to view top 40 games on roblox.
--- server/index.js
+++ server/index.js
@@ -4,39 +4,6 @@ const port = 8000;
 
 app.use(require('express').static('../client/build'));
 
-app.get('/game', (req, res) => {
-  const game = {
-    "data": [
-      {
-        "id": 1659645941,
-        "rootPlaceId": 4872321990,
-        "name": "Islands 🌎 [FURNITURE!]",
-        "description": "Welcome to Islands!\r\n\r\n🌲 Build your own island\r\n👩‍🌾 Create huge farms\r\n💸 Sell items to make money\r\n👨‍🍳 Cook tasty delicacies\r\n✨ Forage valuable items\r\n🔥 And more!\r\n\r\n🛑 If you get stuck on loading screen, type /reset in chat. This WILL reset your island and inventory. Your data cannot be recovered.\r\n\r\n👑 Creators:\r\nSnickTrix (https://twitter.com/lukechatton)\r\nspleenhook (https://twitter.com/spleenhook)\r\n\r\n🎵 Music by: https://twitter.com/LawnReality",
-        "creator": {
-          "id": 5774246,
-          "name": "Easy.gg",
-          "type": "Group"
-        },
-        "price": null,
-        "allowedGearGenres": [
-          "All"
-        ],
-        "allowedGearCategories": [],
-        "playing": 15158,
-        "visits": 99999,
-        "maxPlayers": 15,
-        "created": "2020-04-08T02:47:17.397Z",
-        "updated": "2021-01-15T18:09:41.013Z",
-        "studioAccessToApisAllowed": false,
-        "createVipServersAllowed": true,
-        "universeAvatarType": "MorphToR15",
-        "genre": "All"
-      }
-    ]
-  }
-  res.send(game)
-})
-
 app.get('/list', (req, res) => {
   axios.get('https://games.roblox.com/v1/games/list?model.pageContext.isSeeAllPage=true')
     .then(res => res.data)
@@ -84,8 +51,6 @@ app.get('/getInfo', (req, res) => {
       })
       */
     })
-  // console.log(gameData);
-  // res.send(gameData);
 })
 
 app.listen(port, () => {
